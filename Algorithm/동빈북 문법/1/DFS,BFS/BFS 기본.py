5abd28180d5e0253b3da282d98703175db0e4e72
WOONBE <kepull2918@naver.com>
2023-02-11 19:31:55 +0900

    0211 알고리즘 풀이

---
+++ Algorithm/동빈북 문법/1/DFS,BFS/BFS 기본.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+graph = [[] for _ in range(9)]
+
+visited = [False] * 9
+
+bfs(graph,1,visited)
+
